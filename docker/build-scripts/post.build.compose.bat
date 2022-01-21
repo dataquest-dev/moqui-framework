8afc7fb2cacead9a883e1e5757039212b1ce50aa
Michal Rovnanik on WS <michal@rovnanik.sk>
2022-01-21 09:49:31 +0100

    new compose bat, new Dockerfile

--- docker/build-scripts/post.build.compose.bat
+++ docker/build-scripts/post.build.compose.bat
@@ -20,8 +20,26 @@ pushd ..\simple
 REM unzip built application
 7z x ..\%MOQUI_HOME%\moqui-plus-runtime.war
 
+REM copy source code
+mkdir moqui-source
+xcopy /E /i ..\%MOQUI_HOME%\runtime moqui-source\runtime
+xcopy /E /i ..\%MOQUI_HOME%\framework moqui-source\framework
+xcopy /E /i ..\%MOQUI_HOME%\moqui-util moqui-source\moqui-util
+REM xcopy /E /i ..\%MOQUI_HOME%\gradle moqui-source\gradle
+xcopy ..\%MOQUI_HOME%\*.* moqui-source
+
+REM 1. run database (inside separate component)
+docker run -d --rm --name dev-postgres -e POSTGRES_PASSWORD=postgres -e PGPORT=5431 -e POSTGRES_HOST_AUTH_METHOD=trust -p 5431:5431 --network=dtq-int postgres
+cat ./CreateDatabase.sql | docker exec -i dev-postgres psql -U postgres -d postgres
+
+REM 2. run application component (inside separate component as well) - this is the component that is being built
+
+
+REM RUN IN ENTIRETY
 REM set the project name to 'moqui', network will be called 'moqui_default'
 docker-compose -f ../%COMP_FILE% -p moqui-dynamic --verbose up -d --build
+
+REM docker stop dev-postgres
 
 REM delete all that remains
 rmdir /Q /S META-INF
@@ -30,6 +48,8 @@ rmdir /Q /S execlib
 rmdir /Q /S runtime
 del *.class
 del Procfile
+
+rmdir /Q /S moqui-source
 
 REM return back to original dir
 popd
