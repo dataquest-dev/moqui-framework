7148f441fc5c08e70a3fc250339b59ce929d4d15
Michal Rovnanik on WS <michal@rovnanik.sk>
2022-01-22 20:56:15 +0100

    gradle-tasks folder created inside docker directory, batch file refactored, with docker-compose now used

--- docker/build-scripts/gs-generate-pg-dump.bat
+++ docker/build-scripts/gs-generate-pg-dump.bat
@@ -1,46 +1,25 @@
 ECHO OFF
-ECHO "composing Moqui environment - no DB and no ELASTIC"
-
-SET COMP_FILE=%~1
-SET MOQUI_HOME=%~2
-
-if [%1] == [] (
-    ECHO "setting compose-file to default"
-    SET COMP_FILE="moqui-pg-jdk-11-compose.yml"
-)
-
-if [%2] == [] (
-    ECHO "setting path to default"
-    SET MOQUI_HOME=".."
-)
+ECHO "Creating database dump for purpose of having a clear and filled database"
 
 REM get into the simple-build directory
-pushd ..\simple
+pushd ..\gradle-tasks
 
 REM 1. create database server (inside separate component) and fill it
 docker-compose -p pg-dump   -f create-database.yml build --no-cache
 docker-compose -p pg-dump   -f create-database.yml up -d
 
 REM 2. run application component (inside separate component as well) - this is the component that is being built
-REM this will actually initialize the database
-
-REM fill database
+REM this will actually initialize the database, without running the component
 docker-compose ^
     -p moqui-fill ^
     -f fill-database.yml ^
     build ^
     --no-cache
 
+REM run DUMP script
 cat ./DumpDatabase.sql | docker exec -i dev-postgres su postgres
 
-REM stop temp database
-
-
-REM RUN IN ENTIRETY
-REM set the project name to 'moqui', network will be called 'moqui_default'
-REM docker-compose -f ../%COMP_FILE% -p moqui-dynamic --verbose up -d --build
-
-
+REM kill 'em
 docker-compose -p moqui-fill -f fill-database.yml down --rmi local -v
 docker-compose -p pg-dump -f create-database.yml down --rmi local -v
 
