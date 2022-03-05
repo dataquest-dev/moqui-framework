730b4afdafaefd445993c34ab60295f8c097478d
Michal Rovnanik on WS <michal@rovnanik.sk>
2022-03-05 18:53:45 +0100

    searching for entities fixed, moved QueryHelpers to new package, checking PrimaryKey field now supports checking for field name

--- framework/src/main/groovy/dtq/rockycube/entity/MultiEntityHandler.groovy
+++ framework/src/main/groovy/dtq/rockycube/entity/MultiEntityHandler.groovy
@@ -1,6 +1,5 @@
 package dtq.rockycube.entity
 
-import org.moqui.Moqui
 import org.moqui.context.ExecutionContext
 import org.moqui.impl.entity.EntityDefinition
 import org.moqui.impl.entity.EntityFacadeImpl
@@ -21,11 +20,41 @@ class MultiEntityHandler {
         efi = (EntityFacadeImpl) ec.getEntity()
     }
 
+    private <K, V> Map.Entry<K, V> foundEntity(
+            Iterator iterator,
+            String entityName,
+            String groupName,
+            ArrayList<String> allowedConversions,
+            boolean looseMatch) {
+
+        return iterator.find{ it ->
+            String actualKey = it.key
+            EntityDefinition actualEd = (EntityDefinition) it.value
+            boolean strictMatch = (actualKey == entityName)
+            // if group name set, confirm match
+            if (groupName) strictMatch &= (actualEd.groupName == groupName)
+            if (strictMatch) return true
+
+            // quit if not set to loose match
+            if (!looseMatch) return false
+
+            // convert name to a camel case format
+            return allowedConversions.any { convType ->
+                String convStringValue = this.formattedString(convType, entityName)
+                if (!convStringValue) return false
+                boolean matches = actualKey.endsWith( entityName)
+                if (groupName) matches &= (actualEd.groupName == groupName)
+                return matches
+            }
+        } as Map.Entry<K, V>
+
+    }
+
     public EntityDefinition getDefinition(String entityName, boolean looseMatch = false, String groupName = null)
     {
         def allowedConversions =['underscoredToCamelCase-firstUpper']
 
-        def foundEntity = { it ->
+        /*def foundEntity = { Map.Entry<String, EntityDefinition> it ->
             boolean strictMatch = (it.key == entityName)
             // if group name set, confirm match
             if (groupName) strictMatch &= (it.value.groupName == groupName)
@@ -42,14 +71,24 @@ class MultiEntityHandler {
                 if (groupName) looseMatch &= (it.value.groupName == groupName)
                 return looseMatch
             }
-        } as Map.Entry<String, Object>
+        } as Map.Entry<String, Object>*/
 
         // 1. search among framework entities
-        Map.Entry<String, Object> foundEntityDef = efi.frameworkEntityDefinitions.find {foundEntity}
+        Map.Entry<String, Object> foundEntityDef = foundEntity(
+                efi.frameworkEntityDefinitions.iterator(),
+                entityName,
+                groupName,
+                allowedConversions,
+                looseMatch)
 
         if (!foundEntityDef) {
             // 2. search in normal entities
-            foundEntityDef = efi.entityDefinitionCache.find{foundEntity}
+            foundEntityDef = foundEntity(
+                    efi.entityDefinitionCache.iterator(),
+                    entityName,
+                    groupName,
+                    allowedConversions,
+                    true)
         }
 
         if (!foundEntityDef) return null
