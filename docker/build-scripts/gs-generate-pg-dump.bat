f93b9b66b8f2b49e3dd75d9d49afd271181578ae
Michal Rovnanik on WS <michal@rovnanik.sk>
2022-04-06 21:54:55 +0200

    IP of docker host

--- docker/build-scripts/gs-generate-pg-dump.bat
+++ docker/build-scripts/gs-generate-pg-dump.bat
@@ -8,13 +8,16 @@ REM 1. create database server (inside separate component) and fill it
 docker-compose -p pg-dump   -f create-database.yml build --no-cache
 docker-compose -p pg-dump   -f create-database.yml up -d
 
+
+
 REM 2. run application component (inside separate component as well) - this is the component that is being built
 REM this will actually initialize the database, without running the component
 docker-compose ^
     -p moqui-fill ^
     -f fill-database.yml ^
     build ^
-    --no-cache
+    --no-cache ^
+    --build-arg PG_LOAD_SERVER=host.docker.internal
 
 REM run DUMP script
 type DumpDatabase.sql | docker exec -i dev-postgres su postgres
