7bd358366aba1c3a82241fc5f8860fbf4cbe0196
Michal Rovnanik on WS <michal@rovnanik.sk>
2022-04-11 17:53:16 +0200

    typos

--- docker/gradle-tasks/fill-database/DumpDatabase.sql
+++ docker/gradle-tasks/fill-database/DumpDatabase.sql
@@ -1,3 +1,2 @@
-pg_dump
--U moqui moqui > /tmp/moqui-dump.sql --exclude-table=public.user_account
+pg_dump -U moqui moqui > /tmp/moqui-dump.sql --exclude-table=public.user_account
 pg_dump -U moqui moqui > /tmp/moqui-dump-user.sql --table=public.user_account
