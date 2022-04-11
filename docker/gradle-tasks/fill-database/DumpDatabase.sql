4d0c7e07650672bcca43364ac72e6afc9678f54f
Michal Rovnanik on WS <michal@rovnanik.sk>
2022-04-11 19:27:17 +0200

    back to single dump

--- docker/gradle-tasks/fill-database/DumpDatabase.sql
+++ docker/gradle-tasks/fill-database/DumpDatabase.sql
@@ -1,2 +1,4 @@
-pg_dump -U moqui moqui > /tmp/moqui-dump.sql --exclude-table=public.user_account
-pg_dump -U moqui moqui > /tmp/moqui-dump-user.sql --table=public.user_account
+pg_dump
+-U moqui moqui > /tmp/moqui-dump.sql
+-- pg_dump -U moqui moqui > /tmp/moqui-dump.sql --exclude-table=public.user_account
+-- pg_dump -U moqui moqui > /tmp/moqui-dump-user.sql --table=public.user_account
